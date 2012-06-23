5c6c9c6aac89558bf798a446707fe4b2cce4a9c3
Aseradyn <aseradyn@gmail.com>
2012-06-23 18:14:37 -0500

    Put the connection function back in the main pages
    Wasn't working; should have checked it before committing, right? :oP

--- recurringTasks.php
+++ recurringTasks.php
@@ -487,3 +487,38 @@ function confirm($msg)
 }//end function 
 
 ?>
+
+<?php
+/************************************************************************* 
+               db connection function 
+*************************************************************************/ 
+function conn($sql) 
+{    
+/* 
+  If you use include("/connect.php") delete everyting else within these function bracket 
+  and change $result = conn($sql); to $result = mysql_query($sql) or die; 
+*/ 
+
+$host = "localhost";   // may need to change according your settings 
+$user = "root";        // may need to change according your settings 
+$pass = "";       // may need to change according your settings 
+$db   = "floatyourboat"; 
+
+    //echo "commnecing connection to local db<br>"; 
+    
+    if (!($conn=mysql_connect($host, $user, $pass)))  { 
+        printf("error connecting to DB by user = $user and pwd=$pass"); 
+        exit; 
+    } 
+    $db3=mysql_select_db($db,$conn) or die("Unable to connect to local database"); 
+
+    $result = mysql_query($sql) or die ("Can't run query because ". mysql_error()); 
+    
+    return $result;
+	
+	
+    
+}//end function 
+?>
+
+<?php include 'footer.php'; ?>
